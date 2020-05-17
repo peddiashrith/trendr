a93477900e50bc9aa9e6052f7a7b5147a546a64d
peddiashrith <44424610+peddiashrith@users.noreply.github.com>
2020-05-17 22:17:35 +0530

    added

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'twitter_list'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
